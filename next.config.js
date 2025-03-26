cf8742be8848929a56c80dc36e0e2eb7b82d9623
BharatKumawat63777 <bharatkumawat63777@gmail.com>
2025-03-26 11:35:16 +0530

    next.config.js update because it's not working api route

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export",  // Enables static site generation
+    
     trailingSlash: true,  // Ensures URLs have a trailing slash
     images: {
       unoptimized: true,  // Fixes issues with next/image when exporting statically
