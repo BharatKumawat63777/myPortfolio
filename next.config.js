146cbf6957f9b6ed0dce917335eb9219679b0a7d
BharatKumawat63777 <bharatkumawat63777@gmail.com>
2025-03-26 11:24:43 +0530

    scripts and package.json change then do it again

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export",
-    trailingSlash: true,
+    output: "export",  // Enables static site generation
+    trailingSlash: true,  // Ensures URLs have a trailing slash
     images: {
-      unoptimized: true,
+      unoptimized: true,  // Fixes issues with next/image when exporting statically
     },
   };
   
